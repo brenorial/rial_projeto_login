2b3676226e7a833545656099f8c56e57536d753c
brenorial <brenoalmeida.riosaude@gmail.com>
2025-05-15 21:27:32 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, redirect, request, url_for
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
+from models import Usuario
+from db import db
+import hashlib 
+
+
+app = Flask(__name__)
+app.secret_key = 'brenorial'
+lm = LoginManager(app)
+lm.login_view =  'login'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+db.init_app(app)
+
+
+def hash(txt):
+    hash_obj = hashlib.sha256(txt.encode('utf-8'))
+    return hash_obj.hexdigest()
+
+@lm.user_loader
+def user_loader(id):
+    usuario = db.session.query(Usuario).filter_by(id=id).first()
+    return usuario
+
+
+@app.route('/')
+@login_required
+def home():
+    print(current_user)
+    return render_template('home.html')
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        nome = request.form['nomeForm']
+        senha = request.form['senhaForm']
+
+        user = db.session.query(Usuario).filter_by(nome=nome, senha=hash(senha)).first()
+        if not user:
+            return 'Nome ou senha incorretos'
+        
+        login_user(user)
+        return redirect(url_for('home')) 
+
+
+@app.route('/registrar', methods=['GET','POST'])
+def registrar():
+    if request.method == 'GET':
+        return render_template('registrar.html')
+    elif request.method == 'POST':
+        nome = request.form['nomeForm']
+        senha = request.form['senhaForm']
+
+        novo_usuario = Usuario(nome=nome, senha=hash(senha))
+        db.session.add(novo_usuario)
+        db.session.commit()
+
+        login_user(novo_usuario)
+
+        return redirect(url_for('home')) 
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home')) 
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
